33c95d5b945e60038c25cf0c727d7c7d37e6b01c
Roman Pushkin <roman@breakthrough.com>
2016-02-18 08:59:20 -0800

    Add clean command and reorganize gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,36 @@
 var gulp = require('gulp');
 var server = require('gulp-server-livereload');
+var clean = require('gulp-rimraf');
+var sequence = require('run-sequence');
 
-gulp.task('server', function() {
+gulp.task('build', function(callback) {
+  sequence('clean', ['copy-html', 'browserify'], callback);
+});
+
+  // Clean dist folder
+  gulp.task('clean', function() {
+    return gulp.src('./dist', {
+      read: false
+    }).pipe(clean({
+      force: true
+    }));
+  });
+
+  // copy html files
+  gulp.task('copy-html', function() {
+    return gulp // note "return" keyword
+      .src('./src/**/*.html') // copy everything, including subdirectories
+      .pipe(gulp.dest('dist')); // to dist folder
+  });
+
+  // convert React components to be used in the browser
+  gulp.task('browserify', function() {
+    // nothing here for now
+  });
+
+gulp.task('server', ['build'], function() {
   gulp
-  .src('./src') // everything from ./src directory
+  .src('./dist') // everything from ./dist directory
   .pipe(server({ // pipe to server object
     livereload: true, // reload when files are changed
     open: true // and open the browser
