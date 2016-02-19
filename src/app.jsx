1bbebf329f6266b02777fa906dd198d5c9fc08a0
Roman Pushkin <roman@breakthrough.com>
2016-02-19 12:12:26 -0800

    Add our component and it's dependencies
    WARNING: Example at this point won't work, because you won't have bundle.js in your dist folder.

---
+++ src/app.jsx
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+import Greeter from './greeter';
+
+let greeter = (
+  <Greeter />
+)
+
+ReactDOM.render(greeter, document.getElementById('app'));
---
+++ src/greeter.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Greeter extends React.Component {
+  render() {
+    return <h1>Hello!</h1>;
+  }
+}
+
+export default Greeter;
